c3b63e61774cffddb773ab73dcc2c6e54fa890fa
FRANCESCHINI Marco [IN3] <mfrances@t31.torre.math.unipd.it>
2015-12-15 09:13:12 +0100

    Sistemati errori

--- BankQ/utente.cpp
+++ BankQ/utente.cpp
@@ -1,18 +1,5 @@
 #include "utente.h"
-using namespace std;
-//using namespace std::string;
-//#include <QDate>
-#include <ctime> // Usato per birth
-#include <string>
-
 
 Utente::Utente() {
-    private:
-        string name;
-        string surname;
-        //Qdate birth;
-        string address;
-        int telephone;
-        string code; // Codice fiscale è univoco
 }
 
--- BankQ/utente.h
+++ BankQ/utente.h
@@ -1,10 +1,20 @@
 #ifndef UTENTE_H
 #define UTENTE_H
 
+using namespace std;
+//using namespace std::string;
+//#include <QDate>
+#include <ctime> // Usato per birth
+#include <string>
 
 class Utente {
-    public:
-        Utente();
+    private:
+        string name;
+        string surname;
+        //Qdate birth;
+        string address;
+        int telephone;
+        string code; // Codice fiscale è univoco
 };
 
 #endif // UTENTE_H
