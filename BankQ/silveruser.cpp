63594f2331eaf8591f9f4eefc165a035d87cc0c1
FRANCESCHINI Marco [IN3] <mfrances@t31.torre.math.unipd.it>
2015-12-15 09:22:16 +0100

    Aggiunto utente silver

---
+++ BankQ/silveruser.cpp
@@ -0,0 +1,5 @@
+#include "silveruser.h"
+
+SilverUser::SilverUser()
+{
+}
---
+++ BankQ/silveruser.h
@@ -0,0 +1,11 @@
+#ifndef SILVERUSER_H
+#define SILVERUSER_H
+
+class SilverUser {
+    private:
+        float surcharge; //  L'utente silver ha tasse aggiuntive da pagare
+        float bonus; // L'utente silver ha un bonus aggiuntivo rispetto a quello standard
+
+};
+
+#endif // SILVERUSER_H
