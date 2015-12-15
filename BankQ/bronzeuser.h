3e24674573875c7ea1fb2abf7aba4b827b9daccf
FRANCESCHINI Marco [IN3] <mfrances@t31.torre.math.unipd.it>
2015-12-15 09:18:59 +0100

    Modifica nomi

---
+++ BankQ/bronzeuser.h
@@ -0,0 +1,10 @@
+#ifndef BRONZEUSER_H
+#define BRONZEUSER_H
+
+
+class BronzeUser {
+    private:
+         // float surcharge; L'utente bronzo non ha tasse aggiuntive da pagare a differenza degli altri tipi di utente
+};
+
+#endif // BRONZEUSER_H
